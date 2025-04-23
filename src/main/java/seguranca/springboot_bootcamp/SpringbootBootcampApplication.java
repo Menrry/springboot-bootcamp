8064ddae41c16dcee684b5cb81929028a33bee7b
Menrry <msantanalacruz@gmail.com>
2025-04-23 01:37:08 -0300

    primer commit

---
+++ src/main/java/seguranca/springboot_bootcamp/SpringbootBootcampApplication.java
@@ -0,0 +1,13 @@
+package seguranca.springboot_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seguranca/springboot_bootcamp/SpringbootBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package seguranca.springboot_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
